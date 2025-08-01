4ee9098d056854ae97981fc4bf18917c7f637709
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2025-07-31 21:22:43 -0300

    add write to file functionality

---
+++ functions/write_file_content.py
@@ -0,0 +1,44 @@
+import os
+
+# from config import MAX_CHARS
+
+
+# def get_file_content(working_directory, file_path):
+#     full_path_parent = os.path.abspath(working_directory)
+#     full_path_child = os.path.abspath(os.path.join(working_directory, file_path))
+
+#     if not full_path_child.startswith(full_path_parent):
+#         return f'Error: Cannot list "{file_path}" as it is outside the permitted working directory'
+
+#     if not os.path.isfile(full_path_child):
+#         return f'Error: File not found or is not a regular file: "{file_path}"'
+
+#     try:
+#         with open(full_path_child, "r") as f:
+#             file_content_string = f.read(MAX_CHARS)
+
+#             if os.path.getsize(full_path_child) > MAX_CHARS:
+#                 file_content_string += (
+#                     f'[...File "{file_path}" truncated at 10000 characters]'
+#                 )
+
+#             return file_content_string
+
+#     except Exception as e:
+#         return f"Erro: {e}"
+
+
+def write_file_content(working_directory, file_path, content):
+    full_path_parent = os.path.abspath(working_directory)
+    full_path_child = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not full_path_child.startswith(full_path_parent):
+        return f'Error: Cannot list "{file_path}" as it is outside the permitted working directory'
+
+    try:
+        with open(full_path_child, "w") as f:
+            f.write(content)
+
+            return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except Exception as e:
+        return f"Erro: {e}"
--- tests.py
+++ tests.py
@@ -1,9 +1,33 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
+from functions.write_file_content import write_file_content
 
 
 def test():
 
+    result = write_file_content(
+        "calculator", "lorem.txt", "wait, this isn't lorem ipsum"
+    )
+    print("- Result:")
+    print(result)
+
+    result = write_file_content(
+        "calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"
+    )
+    print("- Result:")
+    print(result)
+
+    result = write_file_content(
+        "calculator", "/tmp/temp.txt", "this should not be allowed"
+    )
+    print("- Result:")
+    print(result)
+
+
+if __name__ == "__main__":
+    test()
+
+"""
     result = get_file_content("calculator", "main.py")
     print("Result for '../' directory:")
     print(result)
@@ -19,7 +43,4 @@ def test():
     result = get_file_content("calculator", "pkg/does_not_exist.py")
     print("Result for '../' directory:")
     print(result)
-
-
-if __name__ == "__main__":
-    test()
+"""
